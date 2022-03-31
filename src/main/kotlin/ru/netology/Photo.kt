cf283af47e40ea83b6ad453c1ea2913acbe9c62e
Alexey Gulyaev <algulaev@mail.ru>
2022-03-31 08:50:58 +0300

    add attachments

---
+++ src/main/kotlin/ru/netology/Photo.kt
@@ -0,0 +1,13 @@
+package ru.netology
+
+class Photo(
+        val id: Int,
+        val albumId: Int,
+        val ownerId: Int,
+        val userId: Int,
+        val text: String,
+        val date: Int,
+        val sizes: Array<Any>?,
+        val width: Int,
+        val height: Int
+)
