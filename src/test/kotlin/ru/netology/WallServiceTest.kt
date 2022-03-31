b056b1f3d0e1af9702a549c28a778780d82f23c1
Alexey Gulyaev <algulaev@mail.ru>
2022-03-31 14:33:54 +0300

    add tests for fun createComment

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -7,7 +7,124 @@ import org.junit.Before
 
 class WallServiceTest {
 
+    @Before
+    fun restoreForCreateCommentException() {
+        WallService.removeAll()
+    }
 
+    @Test(expected = PostNotFoundException::class)
+    fun createCommentException() {
+        WallService.add(
+            Post(
+                id = 1,
+                ownerId = 1,
+                fromId = 1,
+                createdBy = 1,
+                date = 1,
+                text = "Какой-то текст 1",
+                replyOwnerId = 1,
+                replyPostId = 1,
+                friendsOnly = false,
+                comments = null,
+                copyright = "1",
+                likes = null,
+                reposts = null,
+                views = null,
+                postType = "1",
+                postSource = null,
+                attachments = null,
+                geo = null,
+                signerId = 1,
+                copyHistory = null,
+                postponedId = 1,
+                indexPosts = 0
+            )
+        )
+
+        val comment = Comment(
+            id = 44,
+            fromId = 2341234,
+            postId = 2,
+            date = 343212,
+            text = "Comment for post with id=2"
+        )
+
+        WallService.createComment(comment)
+    }
+
+
+    @Before
+    fun restoreForCreateCommentSuccessful() {
+        WallService.removeAll()
+    }
+
+    @Test
+    fun createCommentSuccessful() {
+        WallService.add(
+            Post(
+                id = 1,
+                ownerId = 1,
+                fromId = 1,
+                createdBy = 1,
+                date = 1,
+                text = "Какой-то текст 1",
+                replyOwnerId = 1,
+                replyPostId = 1,
+                friendsOnly = false,
+                comments = null,
+                copyright = "1",
+                likes = null,
+                reposts = null,
+                views = null,
+                postType = "1",
+                postSource = null,
+                attachments = null,
+                geo = null,
+                signerId = 1,
+                copyHistory = null,
+                postponedId = 1,
+                indexPosts = 0
+            )
+        )
+
+        WallService.add(
+            Post(
+                id = 2,
+                ownerId = 1,
+                fromId = 1,
+                createdBy = 1,
+                date = 1,
+                text = "Какой-то текст 2",
+                replyOwnerId = 1,
+                replyPostId = 1,
+                friendsOnly = false,
+                comments = null,
+                copyright = "1",
+                likes = null,
+                reposts = null,
+                views = null,
+                postType = "1",
+                postSource = null,
+                attachments = null,
+                geo = null,
+                signerId = 1,
+                copyHistory = null,
+                postponedId = 1,
+                indexPosts = 0
+            )
+        )
+
+        val comment = Comment(
+            id = 12,
+            fromId = 123412432,
+            postId = 2,
+            date = 343212,
+            text = "Comment for post with id=2"
+        )
+
+        val result = WallService.createComment(comment)
+        assertTrue(result)
+    }
 
 
     @Before
